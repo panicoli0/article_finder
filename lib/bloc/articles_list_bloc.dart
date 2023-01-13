1c3aee4883fbaf11c21b4112804c4c07414e6c61
Pablo Ariel Nicoli <pablonicoli21@gmail.com>
2023-01-13 12:48:14 +0400

    Added initial bloc structure

---
+++ lib/bloc/articles_list_bloc.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+
+import 'package:article_finder/bloc/bloc.dart';
+
+import '../data/article.dart';
+import '../data/rw_client.dart';
+
+class ArticleListBloc implements Bloc {
+  // 1
+  final _client = RWClient();
+  // 2
+  final _searchQueryController = StreamController<String?>();
+  // 3
+  Sink<String?> get searchQuery => _searchQueryController.sink;
+  // 4
+  late Stream<List<Article>?> articlesStream;
+
+  ArticleListBloc() {
+    // 5
+    articlesStream = _searchQueryController.stream
+        .asyncMap((query) => _client.fetchArticles(query));
+  }
+
+  // 6
+  @override
+  void dispose() {
+    _searchQueryController.close();
+  }
+}
